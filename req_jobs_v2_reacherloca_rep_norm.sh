4f75d9b977ad480a2d02a179a6e8edb72848dcf2
Ruis MacDonald <rs660553@dal.ca>
2026-02-21 19:08:05 -0400

    fixed mem in slurm

--- req_jobs_v2_reacherloca_rep_norm.sh
+++ req_jobs_v2_reacherloca_rep_norm.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --exclude=ng[11105,30708]
-#SBATCH --mem=62G
+#SBATCH --mem=32G
 #SBATCH --array=0
 #SBATCH --acctg-freq=task=1
 #SBATCH --output=/home/ruism/projects/def-rsdjjana/ruism/Dreamer_v2_AEH/results/reacherloca_v2_rep_norm_state_ae/%A-%a.out
